9ad63944307545537de61b45e7b9dd65997e156b
DinandCode <dinandaristo@gmail.com>
2025-12-14 21:37:20 +0700

    first

---
+++ layers/JlnDesa_33.js
@@ -0,0 +1,1 @@
+var json_JlnDesa_33 = {"type":"FeatureCollection","name":"JlnDesa_33","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
---
+++ styles/Balaidesa_31_style.js
@@ -0,0 +1,32 @@
+var size = 0;
+var placement = 'point';
+
+var style_Balaidesa_31 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "13.0px \'Rockwell Extra Bold\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("nama") !== null) {
+        labelText = String(feature.get("nama"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 5.199999999999999 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(183,72,75,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
